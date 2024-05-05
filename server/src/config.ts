f91f2b75e5fc67793ae6f1d0147523af49f74a69
Ankit Chaudhary <ankitcha810@gmail.com>
2024-05-05 18:08:08 +0530

    Fix: config

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,6 +6,6 @@ export const MONGODB_URI = process.env.MONGODB_URI;
 
 export const aws_access_key = process.env.aws_access_key;
 
-export const aws_secret_key = "r2BBiGEOQbYgBLHznSTnGWiCgT3BkfHF7oD4ZTHo";
+export const aws_secret_key = process.env.aws_secret_key;
 
 export const region = "ap-south-1";
