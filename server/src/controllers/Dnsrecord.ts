4a0cd6fcd477661d50bea49fc5b5227bdbfd5f54
Ankit Chaudhary <ankitcha810@gmail.com>
2024-05-06 10:50:06 +0530

    Added isExistingDnsRecords method

--- server/src/controllers/Dnsrecord.ts
+++ server/src/controllers/Dnsrecord.ts
@@ -3,6 +3,7 @@ import {
   ChangeResourceRecordSetsCommand,
   ChangeResourceRecordSetsCommandInput,
   ListResourceRecordSetsCommand,
+  RRType,
 } from "@aws-sdk/client-route-53";
 import { Request, Response, NextFunction } from "express";
 import aws_route53_client from "../utility";
@@ -42,7 +43,6 @@ export const createSingleDnsRecord = async (req: Request, res: Response) => {
   if (req.method === "POST") {
     try {
       const { Name, Type, TTL, resourceRecords, HostedZoneId } = req.body;
-
       const params: ChangeResourceRecordSetsCommandInput = {
         HostedZoneId,
         ChangeBatch: {
@@ -80,4 +80,69 @@ export const createSingleDnsRecord = async (req: Request, res: Response) => {
   } else {
     res.status(405).json({ error: `${req.method} Method not allowed` });
   }
+};
+
+export const DeleteDnsRecord = async (req: Request, res: Response) => {
+  if (req.method === "POST") {
+    try {
+      const { Name, Type, TTL, resourceRecords, HostedZoneId } = req.body;
+      const params: ChangeResourceRecordSetsCommandInput = {
+        HostedZoneId,
+        ChangeBatch: {
+          Changes: [
+            {
+              Action: "CREATE" as ChangeAction,
+              ResourceRecordSet: {
+                Name,
+                Type,
+                TTL,
+                ResourceRecords: [
+                  {
+                    Value: resourceRecords[0].Value,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      };
+
+      const command = new ChangeResourceRecordSetsCommand(params);
+      const response = await aws_route53_client.send(command);
+
+      res.status(200).json({
+        message: "DNS Record created successfully",
+        data: response,
+      });
+    } catch (error) {
+      console.error("Error while creating single DNS records: ", error);
+      res
+        .status(500)
+        .json({ error: "Error while creating single DNS records" });
+    }
+  } else {
+    res.status(405).json({ error: `${req.method} Method not allowed` });
+  }
+};
+
+export const isExistingDnsRecords = async (
+  name: string,
+  type: string,
+  HostedZoneId: string
+) => {
+  const params = {
+    HostedZoneId,
+    MaxItems: 1,
+    StartRecordName: name,
+    StartRecordType: type as RRType,
+  };
+
+  const command = new ListResourceRecordSetsCommand(params);
+  const { ResourceRecordSets } = await aws_route53_client.send(command);
+
+  const dnsRecords = ResourceRecordSets?.filter(
+    (record) => record.Name === name && record.Type === type
+  );
+
+  return dnsRecords;
 };
