d5bb1b834bd97d80fcbba5f0b05aba4822db0f06
Ankit Chaudhary <ankitcha810@gmail.com>
2024-05-07 10:36:26 +0530

    Setup react-query

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,7 +1,35 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="grid grid-cols-12 h-full w-full">
+      {/* lef side */}
+      <div className="grid col-span-3 h-full w-full p-3 ">
+        <div className="bg-slate-100 w-full h-full p-3 rounded-xl  border border-3 border-zinc-600">
+          <p className="font-medium text-sm">Domain Creation</p>
+
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Enter domain Name"
+              className="mt-1 w-full outline-none bg-teal-50 p-2 rounded-lg "
+            />
+            <button className="bg-teal-400 p-2 mt-1 rounded-sm text-white">
+              Create
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className="grid col-span-9 h-full w-full">
+        <div className="bg-slate-100 w-full text-sm h-full p-2 rounded-xl font-medium border border-3 border-zinc-600">
+          Upload Section.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,8 +2,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import HomeImage from "../public/home.jpg";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
       <div className="px-4 md:px-8 lg:px-12 xl:px-16 flex flex-col lg:flex-row justify-between gap-8">
@@ -13,7 +15,7 @@ export default function Home() {
         </div>
 
         <div className="w-full lg:w-1/2 sm:mt-28 mt-1">
-          <h1 className="text-3xl lg:text-4xl font-bold mb-4 lg:mb-6">
+          <h1 className="text-3xl lg:text-4xl font-bold mb-4 lg:mb-6 text-nowrap">
             Be Productive to Your{" "}
             <span className="bg-[#7D96F0] rounded-xl px-2 py-1 text-xl justify-start items-center">
               DNS
@@ -27,6 +29,9 @@ export default function Home() {
           <Button
             className="text-black hover:text-white transition-all hover:bg-[#7D96F0]"
             variant="outline"
+            onClick={() => {
+              router.push("/dashboard");
+            }}
           >
             Continue.
           </Button>
