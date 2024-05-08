c893a456b75daa40d62bc7515c62cd883a57e71d
Ankit Chaudhary <ankitcha810@gmail.com>
2024-05-08 07:48:49 +0530

    Added Responsive ui on client

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -32,9 +32,9 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="grid grid-cols-12 h-full w-full">
+    <div className="grid grid-cols-1 md:grid-cols-12 h-full w-full">
       {/* Left side */}
-      <div className="col-span-3 h-full w-full p-3">
+      <div className="col-span-1 sm:col-span-3 h-full w-full p-2">
         <div className="bg-slate-100 w-full h-full p-3 rounded-xl border border-3 border-zinc-600">
           <p className="font-medium text-sm">Domain Creation</p>
           <div className="flex gap-2">
@@ -56,12 +56,12 @@ const Dashboard = () => {
       </div>
 
       {/* Right side */}
-      <div className="col-span-9 h-full w-full">
+      <div className="col-span-1 md:col-span-9 h-full w-full  p-2">
         <div className="bg-slate-100 w-full text-sm h-full p-2 rounded-xl font-medium border border-3 border-zinc-600">
           <h2 className="text-lg">Domains List</h2>
           <ul>
-            {domain?.map((dom: any) => (
-              <ul>{dom.Name}</ul>
+            {domain?.map((dom: Domain) => (
+              <li key={domain}>{dom.Name}</li>
             ))}
           </ul>
         </div>
